7053ae325f828b1b90d29122f6f65edbcdd10955
billyalbert <30498487+billyalbert@users.noreply.github.com>
2017-11-16 21:59:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,13 +1,13 @@
 <?php
 /*
-copyright @ medantechno.com
-Modified by Ilyasa
+copyright @ Billy Albert
+Modified by Billy Albert
 2017
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //Your Channel Access Token
-$channelSecret = 'YOUR-CHANNEL-SECRET';//Your Channel Secret
+$channelAccessToken = '1546080629'; //Your Channel Access Token
+$channelSecret = 'f04329bb422b7a2ed57bb814f0fda57d';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = '9f256a21-5d2f-4ae2-874a-e500a50bb04c'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
